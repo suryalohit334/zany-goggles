6568f6dc46cf7cbafa1221e2fda5e3ea4614bd22
suryalohit334 <devikagoud245@gmail.com>
2024-07-29 03:07:48 +0000

    nbv

--- app.py
+++ app.py
@@ -3,7 +3,21 @@ import requests
 import time
 import logging
 import sys
+logging.basicConfig(
+     filename='log_file_name.log',
+     level=logging.INFO, 
+     format= '[%(asctime)s] {%(pathname)s:%(lineno)d} %(levelname)s - %(message)s',
+     datefmt='%H:%M:%S'
+ )
 
+# set up logging to console
+console = logging.StreamHandler()
+console.setLevel(logging.DEBUG)
+# set a format which is simpler for console use
+formatter = logging.Formatter('%(name)-12s: %(levelname)-8s %(message)s')
+console.setFormatter(formatter)
+# add the handler to the root logger
+logging.getLogger('').addHandler(console)
 
 
 TOKEN = "6994416717:AAH_qEF1vSy1gZc1nXQ4eyM4dErJshFGJaM"
@@ -13,6 +27,11 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     for i in range(1000):
+        logging.debug('debug')
+        logging.info('info')
+        logging.warning('warning')
+        logging.error('error')
+        logging.exception('exp')
         print("output")
         message=f"run::{i}"
         url = f"https://api.telegram.org/bot{TOKEN}/sendMessage?chat_id={chat_id}&text={message}"
